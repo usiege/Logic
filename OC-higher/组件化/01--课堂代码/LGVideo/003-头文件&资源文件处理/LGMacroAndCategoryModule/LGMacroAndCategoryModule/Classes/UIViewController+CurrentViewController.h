f9cb3857338c75514cbd80814ebb441744b52356
charles <2275572017@qq.com>
2020-12-04 21:19:12 +0800

    LGVideo

---
+++ OC-higher/组件化/01--课堂代码/LGVideo/003-头文件&资源文件处理/LGMacroAndCategoryModule/LGMacroAndCategoryModule/Classes/UIViewController+CurrentViewController.h
@@ -0,0 +1,18 @@
+//
+//  UIViewController+CurrentViewController.h
+//  LGMacroAndCategoryModule
+//
+//  Created by vampire on 2020/3/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (CurrentViewController)
+
++ (UIViewController *)currentViewController;
+
+@end
+
+NS_ASSUME_NONNULL_END
